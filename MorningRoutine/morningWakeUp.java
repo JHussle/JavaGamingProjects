57577046efe1383f1d86b1cd4cc3f4216900ac82
JSmith <65369149+JHussle@users.noreply.github.com>
2021-05-07 18:39:19 -0500

    Updated to the questions

---
+++ MorningRoutine/morningWakeUp.java
@@ -0,0 +1,31 @@
+package MorningRoutine;
+
+import java.util.Scanner;
+
+public class morningWakeUp {
+    public static void main(String[] args) {
+        //Display message that the alarm clock is going off
+
+        Scanner user_input = new Scanner(System.in);
+
+        String firstName;
+        String answer;
+
+        
+
+        System.out.println("What is your name?");
+        firstName = user_input.next();
+
+        System.out.println("Good Morning " + firstName + "!");
+        System.out.println(firstName + " .Would you like to start your day?");  
+        
+        answer = user_input.nextLine();
+        if (answer == "Yes") 
+            System.out.println("Wonder" + firstName +"! What would you like to do first?");
+        if (answer == "No") 
+            System.out.println("Okay," + firstName +". I will try again later.");
+        /* Lines 21-27 are not working,
+        the next question isnt being asked,
+        */
+    }
+}
