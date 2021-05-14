d34584068cc748ff49365c71b19e41a940617dcc
JSmith <65369149+JHussle@users.noreply.github.com>
2021-05-13 19:08:34 -0500

    update to weapons

---
+++ AgeOfWar/Weapons/bow.java
@@ -0,0 +1,11 @@
+package AgeOfWar.Weapons;
+
+public class bow extends weapon {
+    public bow() {
+        name = "Long Bow";
+        attack = 25;
+        range = 10;
+        accuracy = 80;
+        
+    }
+}
